7699efcddd5b181afb1ad720823a5977e8b3d73b
Tiago Teles <tiago.sequeira.teles@gmail.com>
2021-12-14 16:16:15 +0000

    Server side responses

--- server/index.js
+++ server/index.js
@@ -15,11 +15,42 @@ server.listen(port, () => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    let roomId = () => {
+        for (r of socket.rooms.keys()) {
+            if (r != socket.id) {
+                return r;
+            }
+        }
+        throw "No room id";
+    }
+
     socket.on('disconnect', () => {
+        socket.leave(roomId());
         console.log('user disconnected');
     });
 
-    socket.on("join", (roomId) => {
-        socket.join(roomId);
+    socket.on("join", (roomIdNow) => {
+        socket.join(roomIdNow);
+    });
+
+
+    socket.on("play", () => {
+        socket.to(roomId()).emit("play");
+    });
+
+    socket.on("pause", () => {
+        socket.to(roomId()).emit("pause");
+    });
+
+    socket.on("seek", (timestamp) => {
+        socket.to(roomId()).emit("seek", timestamp);
+    });
+
+    socket.on("url", (url) => {
+        socket.to(roomId()).emit("url", url);
+    });
+
+    socket.on("subtitles", (roomIdNow) => {
+        //TODO
     });
 });
