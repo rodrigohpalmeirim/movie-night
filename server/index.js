6be6df4cc41350c851a82d13ca4834ebd7f2fccc
ttmx <git@tteles.dev>
2021-12-15 02:15:56 +0000

    State on server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const { time } = require('console');
 const io = new Server(server);
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -13,46 +14,67 @@ server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-rooms = {};
+let rooms = {};
 
 io.on('connection', (socket) => {
-    socket.onAny((...a)=>console.log(a))
+    socket.onAny((...a) => console.log(a));
     console.log('a user connected');
 
     let room;
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        io.in(room).fetchSockets().then(sockets => io.in(room).emit("people", sockets.length));
+        io.in(roomId).fetchSockets().then(sockets => io.in(roomId).emit("people", sockets.length));
     });
 
-    socket.on("join", (roomId) => {
-        room = roomId;
-        if (rooms[room] == undefined) {
-            rooms[room] = {
+    socket.on("join", (_roomId) => {
+        if (rooms[_roomId] == undefined) {
+            room = {
+                id: _roomId,
+                playing: false,
+                lastKnownSeek: 0,
+                lastServerTime: new Date(),
                 buffering: 0,
+                url: null,
+                time: () => (playing ? (new Date() - room.lastServerTime) : 0) + lastKnownSeek,
+                getNumPeople: () => io.in(room.id).fetchSockets().then(sockets => io.in(room.id).emit("people", sockets.length))
             };
+            rooms[room.id] = room;
+        } else {
+            room = rooms[_roomId];
         }
-        socket.join(room);
+        socket.join(room.id);
 
-        io.in(room).fetchSockets().then(sockets => io.in(room).emit("people", sockets.length));
+        ((playing) ? socket.to(room.id) : socket).emit("pause", time);
+
+
+        io.in(room.id).fetchSockets().then(sockets => io.in(room.id).emit("people", sockets.length));
     });
 
 
-    socket.on("play", () => {
-        socket.to(room).emit("play");
+    socket.on("play", (timestamp) => {
+        socket.to(room.id).emit("play");
+        room.playing = true;
+        room.lastKnownSeek = timestamp;
+        room.lastServerTime = new Date();
     });
 
-    socket.on("pause", () => {
-        socket.to(room).emit("pause");
+    socket.on("pause", (timestamp) => {
+        socket.to(room.id).emit("pause", timestamp);
+        room.playing = false;
+        room.lastKnownSeek = timestamp;
+        room.lastServerTime = new Date();
     });
 
     socket.on("seek", (timestamp) => {
-        socket.to(room).emit("seek", timestamp);
+        socket.to(room.id).emit("seek", timestamp);
+        room.lastKnownSeek = timestamp;
+        room.lastServerTime = new Date();
     });
 
     socket.on("url", (url) => {
-        socket.to(room).emit("url", url);
+        socket.to(room.id).emit("url", url);
+        room.url = url;
     });
 
     socket.on("subtitles", () => {
