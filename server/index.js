2d19641e6383c37364e5d59d04d53292e6fbd709
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-12-17 21:46:25 +0000

    fix path

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(cors());
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '../client/build/index.html');
+    res.sendFile(__dirname + '/../client/build/index.html');
 });
 
 server.listen(port, () => {
