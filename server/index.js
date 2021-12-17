22fa3a815ed8a2a6b14e28cddb1dffe72a1c8608
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-12-17 18:58:42 +0000

    catch all exceptions

--- server/index.js
+++ server/index.js
@@ -24,109 +24,115 @@ io.on('connection', (socket) => {
     let room;
 
     let ready = false;
-    socket.on("join", (_roomId) => {
-        if (rooms[_roomId] == undefined) {
-            room = {
-                id: _roomId,
-                playing: true,
-                lastKnownSeek: 0,
-                lastServerTime: new Date(),
-                buffering: 0,
-                url: null,
-                subtitles: [],
-                numPeople: 1,
-                time: () => (room.playing ? (new Date() - room.lastServerTime) / 1000 : 0) + room.lastKnownSeek,
-            };
-            rooms[room.id] = room;
-        } else {
-            room = rooms[_roomId];
-            room.numPeople++;
-            room.buffering = room.numPeople;
-        }
-        socket.join(room.id);
 
-        socket.emit("url", room.url);
-        io.in(room.id).emit("people", room.numPeople);
-        io.in(room.id).emit("seek", room.time());
-        io.in(room.id).emit("buffering", room.buffering);
-        io.in(room.id).emit("subtitles", room.subtitles);
-
-        // io.in(room.id).fetchSockets().then(sockets => io.in(room.id).emit("people", sockets.length));
-
-        console.log('a user joined');
-        console.log("room", room)
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        if (room != undefined) {
-            console.log(room);
-            // io.in(room.id).fetchSockets().then(sockets => io.in(room.Id).emit("people", sockets.length));
-            room.numPeople--;
-            io.in(room.id).emit("people", room.numPeople);
-            if (!ready) {
-                room.buffering = Math.max(room.buffering - 1, 0);
-                io.in(room.id).emit("buffering", room.buffering);
+    try {
+        socket.on("join", (_roomId) => {
+            if (rooms[_roomId] == undefined) {
+                room = {
+                    id: _roomId,
+                    playing: true,
+                    lastKnownSeek: 0,
+                    lastServerTime: new Date(),
+                    buffering: 0,
+                    url: null,
+                    subtitles: [],
+                    numPeople: 1,
+                    time: () => (room.playing ? (new Date() - room.lastServerTime) / 1000 : 0) + room.lastKnownSeek,
+                };
+                rooms[room.id] = room;
+            } else {
+                room = rooms[_roomId];
+                room.numPeople++;
+                room.buffering = room.numPeople;
             }
+            socket.join(room.id);
+
+            socket.emit("url", room.url);
+            io.in(room.id).emit("people", room.numPeople);
+            io.in(room.id).emit("seek", room.time());
+            io.in(room.id).emit("buffering", room.buffering);
+            io.in(room.id).emit("subtitles", room.subtitles);
+
+            // io.in(room.id).fetchSockets().then(sockets => io.in(room.id).emit("people", sockets.length));
+
+            console.log('a user joined');
+            console.log("room", room)
+        });
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+            if (room != undefined) {
+                console.log(room);
+                // io.in(room.id).fetchSockets().then(sockets => io.in(room.Id).emit("people", sockets.length));
+                room.numPeople--;
+                io.in(room.id).emit("people", room.numPeople);
+                if (!ready) {
+                    room.buffering = Math.max(room.buffering - 1, 0);
+                    io.in(room.id).emit("buffering", room.buffering);
+                }
+                if (room.buffering <= 0 && room.playing) {
+                    io.in(room.id).emit("play");
+                    room.lastServerTime = new Date();
+                }
+            }
+        });
+
+        socket.on("play", () => {
+            room.playing = true;
+            if (room.buffering <= 0) {
+                socket.to(room.id).emit("play");
+                room.lastServerTime = new Date();
+            }
+        });
+
+        socket.on("pause", (timestamp) => {
+            socket.to(room.id).emit("pause", timestamp);
+            room.buffering = room.numPeople - 1;
+            room.playing = false;
+            ready = false;
+            room.lastKnownSeek = timestamp;
+            room.lastServerTime = new Date();
+            io.in(room.id).emit("buffering", room.buffering);
+        });
+
+        socket.on("seek", (timestamp) => {
+            ready = false;
+            socket.to(room.id).emit("seek", timestamp);
+            room.buffering = room.numPeople;
+            room.lastKnownSeek = timestamp;
+            room.lastServerTime = new Date();
+            io.in(room.id).emit("buffering", room.buffering);
+        });
+
+        socket.on("url", (url) => {
+            ready = false;
+            socket.to(room.id).emit("url", url);
+            room.buffering = room.numPeople;
+            room.lastKnownSeek = 0;
+            room.lastServerTime = new Date();
+            room.url = url;
+        });
+
+        socket.on("ready", () => {
+            ready = true;
+            room.buffering = Math.max(room.buffering - 1, 0);
+            io.in(room.id).emit("buffering", room.buffering);
             if (room.buffering <= 0 && room.playing) {
                 io.in(room.id).emit("play");
                 room.lastServerTime = new Date();
             }
-        }
-    });
+        });
 
-    socket.on("play", () => {
-        room.playing = true;
-        if (room.buffering <= 0) {
-            socket.to(room.id).emit("play");
-            room.lastServerTime = new Date();
-        }
-    });
+        socket.on("subtitles", (subtitles) => {
+            socket.to(room.id).emit("subtitles", subtitles);
+            room.subtitles = subtitles;
+        });
 
-    socket.on("pause", (timestamp) => {
-        socket.to(room.id).emit("pause", timestamp);
-        room.buffering = room.numPeople - 1;
-        room.playing = false;
-        ready = false;
-        room.lastKnownSeek = timestamp;
-        room.lastServerTime = new Date();
-        io.in(room.id).emit("buffering", room.buffering);
-    });
-
-    socket.on("seek", (timestamp) => {
-        ready = false;
-        socket.to(room.id).emit("seek", timestamp);
-        room.buffering = room.numPeople;
-        room.lastKnownSeek = timestamp;
-        room.lastServerTime = new Date();
-        io.in(room.id).emit("buffering", room.buffering);
-    });
-
-    socket.on("url", (url) => {
-        ready = false;
-        socket.to(room.id).emit("url", url);
-        room.buffering = room.numPeople;
-        room.lastKnownSeek = 0;
-        room.lastServerTime = new Date();
-        room.url = url;
-    });
-
-    socket.on("ready", () => {
-        ready = true;
-        room.buffering = Math.max(room.buffering - 1, 0);
-        io.in(room.id).emit("buffering", room.buffering);
-        if (room.buffering <= 0 && room.playing) {
-            io.in(room.id).emit("play");
-            room.lastServerTime = new Date();
-        }
-    });
-
-    socket.on("subtitles", (subtitles) => {
-        socket.to(room.id).emit("subtitles", subtitles);
-        room.subtitles = subtitles;
-    });
-
-    socket.on("info", (roomId, callback) => {
-        callback({ people: rooms[roomId] !== undefined ? rooms[roomId].numPeople : 0 });
-    })
+        socket.on("info", (roomId, callback) => {
+            callback({ people: rooms[roomId] !== undefined ? rooms[roomId].numPeople : 0 });
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
 });
