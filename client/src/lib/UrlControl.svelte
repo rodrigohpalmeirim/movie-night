765a87f8989af0270688d28421d6bea70e346eb3
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-04-26 16:24:34 +0100

    fix empty URL input not checked

--- client/src/lib/UrlControl.svelte
+++ client/src/lib/UrlControl.svelte
@@ -7,7 +7,7 @@
 
     let input;
     function checkAndSetUrl() {
-        if (input.checkValidity()) {
+        if (input.value && input.checkValidity()) {
             setUrl(input.value);
             openMenu = "url";
         } else {
@@ -25,7 +25,7 @@
 <div bind:this={menu} class="relative w-8 group transition-all">
     <div class="{openMenu == "url" ? "opacity-100 scale-100":"opacity-0 scale-75"} flex gap-1 absolute -right-10 bottom-10 w-48 h-10 bg-slate-800 rounded-2xl transition-all overflow-hidden p-1">
         <input 
-            pattern="https?://.+\.(mp4|webm|ogg)(\?.*)?$"
+            pattern="^https?://.+\.(mp4|webm|ogg)(\?.*)?$"
             placeholder={url}
             bind:this={input} on:keydown={(e)=>{if (e.key === "Enter") checkAndSetUrl()}} type="text"
             class="w-full bg-slate-900 text-slate-200 p-2 rounded-xl outline-slate-600 outline-2 focus:outline invalid:outline-rose-300"
