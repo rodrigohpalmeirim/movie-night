67449826b97605a8d2b131d1288919dfd6cd5b88
rodrigohpalmeirim <rod@rodlabs.dev>
2026-08-09 19:08:50 +0100

    update app icon and favicon

--- src/routes/g/[token]/manifest.webmanifest/+server.ts
+++ src/routes/g/[token]/manifest.webmanifest/+server.ts
@@ -48,7 +48,7 @@ const STATIC_MANIFEST = {
 		{ src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
 		{ src: '/icon-maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
 		{ src: '/icon-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
-		{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any' }
+		{ src: '/icon-rounded.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any' }
 	]
 } as const;
 
